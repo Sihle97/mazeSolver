9eafd1fbf3572fe6655c85ae959de056062ae426
sihle <sihlesakhile97@gmail.com>
2022-06-22 20:48:17 +0200

    Initializing class and checking whether it's walkable

--- Node.rb
+++ Node.rb
@@ -1,7 +1,20 @@
 class Node
 
     def initialize(type,x,y)
-
+        @type = type
+        @x = x
+        @y = y
     end
-    
+
+    def walkable?
+        if self.type == '*'
+            return false
+        else
+            return true
+        end
+    end
+
+    def type
+        @type
+    end
 end
