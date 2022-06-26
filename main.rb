fd27fb992ceab361f5b910cb67237836c9324462
sihle <sihlesakhile97@gmail.com>
2022-06-26 02:32:24 +0200

    Game implementation

--- main.rb
+++ main.rb
@@ -1,3 +1,15 @@
-class Main
+require_relative 'maze.rb'
 
+
+game = Maze.new('maze.txt')
+
+while !game.hasE? 
+    game.search(game.currentNode)
+
+    game.lowF
 end
+
+game.findPath
+game.writeOutput
+
+
